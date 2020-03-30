ce8ffddfb13820cf8e6332e0eb752bb6523d5243
Viktor <viktor@intrykk.se>
2020-03-30 02:15:02 +0200

    Add environment variables

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_SITE_TITLE: string;
+    REACT_APP_SITE_DESCRIPTION: string;
+    REACT_APP_PUBLISHER: string;
+    REACT_APP_COPYRIGHT_FROM_YEAR: number;
+    REACT_APP_YOUTUBE_API_KEY: string;
+    REACT_APP_YOUTUBE_CHANNEL_ID: string;
+    REACT_APP_YOUTUBE_UNLISTED_PLAYLISTS_IDS: string;
+    REACT_APP_YOUTUBE_FEATURED_VIDEO_ID: string;
+  }
+}
